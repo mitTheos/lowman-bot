cd018dbf641f6458601eba3e63d62e6f93f83cfa
Tim <33898974+TimKaspar@users.noreply.github.com>
2022-11-25 10:23:06 +0100

    formatting

--- src/api/lowmanVerifierAPI.js
+++ src/api/lowmanVerifierAPI.js
@@ -5,7 +5,6 @@ module.exports = { getLowmans };
 async function getLowmans(username) {
   let res = await axios.get(`https://lowman-verifier.herokuapp.com/lowmans/${username}`);
   let data = res.data;
-
   console.log(data);
   return data;
 }
