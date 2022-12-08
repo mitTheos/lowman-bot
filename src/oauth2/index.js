1d58ed8df52c59f194998cf9d3d1c7fcf8b6e2ba
Tim <33898974+TimKaspar@users.noreply.github.com>
2022-12-08 13:47:03 +0100

    bungie auth working fully
    discord auth still only works on http for reasons

--- src/oauth2/index.js
+++ src/oauth2/index.js
@@ -1,47 +1,43 @@
-const { request } = require('undici');
-const express = require('express');
-const { discordClientId, discordClientSecret,bungieClientId, bungieClientSecret, port } = require('./config.json');
-const fs = require('fs');
+const { request } = require("undici");
+const express = require("express");
+const { discordClientId, discordClientSecret, bungieClientId, bungieClientSecret, port, apiKey } = require("./config.json");
+const fs = require("fs");
 const https = require("https");
 const options = {
-  key : fs.readFileSync("src/oauth2/config/cert.key"),
-  cert : fs.readFileSync("src/oauth2/config/cert.crt")
-}
+  key: fs.readFileSync("C:/Users/kti/Documents/ssl-certificate/cert.key"),
+  cert: fs.readFileSync("C:/Users/kti/Documents/ssl-certificate/cert.crt"),
+};
 
 const app = express();
-const server = https.createServer(options,app)
+const server = https.createServer(options, app);
 
-
-app.use("/", express.static("public"));
-
-app.get('/', async ({ query }, response) => {
+app.get("/", async ({ query }, response) => {
   const { code } = query;
 
   if (code) {
     try {
-      const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
-        method: 'POST',
+      const tokenResponseData = await request("https://discord.com/api/oauth2/token", {
+        method: "POST",
         body: new URLSearchParams({
           client_id: discordClientId,
           client_secret: discordClientSecret,
           code,
-          grant_type: 'authorization_code',
+          grant_type: "authorization_code",
           redirect_uri: `https://localhost:${port}`,
-          scope: 'identify',
+          scope: "identify"
         }).toString(),
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
       });
 
       const oauthData = await tokenResponseData.body.json();
       console.log(oauthData);
-
-      const userResult = await request('https://discord.com/api/users/@me', {
+      const userResult = await request("https://discord.com/api/users/@me", {
         headers: {
-          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-        },
-      });
+          authorization: `${oauthData.token_type} ${oauthData.access_token}`
+        }
+      }).catch(console.error);
 
       console.log(await userResult.body.json());
     } catch (error) {
@@ -51,44 +47,46 @@ app.get('/', async ({ query }, response) => {
     }
   }
 
-  return response.sendFile('src/oauth2/index.html', { root: '.' });
+  return response.sendFile("src/oauth2/index.html", { root: "." });
 });
 
-app.get('/bungie/', async ({ query }, response) => {
+app.get("/bungie/", async ({ query }, response) => {
   const { code } = query;
 
   if (code) {
     try {
-      const tokenResponseData = await request('https://www.bungie.net/platform/app/oauth/token/', {
-        method: 'POST',
+      const tokenResponseData = await request("https://www.bungie.net/platform/app/oauth/token/", {
+        method: "POST",
         body: new URLSearchParams({
           client_id: bungieClientId,
+          client_secret: bungieClientSecret,
           code,
-          grant_type: 'authorization_code',
-          redirect_uri: `https://localhost:${port}/bungie`,
+          grant_type: "authorization_code",
+          redirect_uri: `https://localhost:${port}/bungie`
         }).toString(),
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
       });
 
       const oauthData = await tokenResponseData.body.json();
-      console.log(oauthData);
-
-      const userResult = await request('https://discord.com/api/users/@me', {
+      const userResult = await request("https://www.bungie.net/Platform/User/GetCurrentBungieAccount/", {
         headers: {
-          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-        },
-      });
+          "X-API-Key": `${apiKey}`,
+          Authorization: `${oauthData.token_type} ${oauthData.access_token}`
+        }
+      }).catch(console.error);
+      const response = await userResult.body.json();
+      const destinyMembershipId = Object.values(response["Response"]["membershipOverrides"])[0]["membershipIdOverriding"]
+      console.log(`D2MembershipId: ${destinyMembershipId}`)
 
-      console.log(await userResult.body.json());
     } catch (error) {
       // NOTE: An unauthorized token will not throw an error
       // tokenResponseData.statusCode will be 401
       console.error(error);
     }
   }
-
-  return response.sendFile('src/oauth2/index.html', { root: '.' });
+  return response.sendFile("src/oauth2/index.html", { root: "." });
 });
+
 server.listen(port, () => console.log(`Server started at https://localhost:${port}`));
