28084cb68da70a88a0ad9517dbe900f2d633e978
Tim <33898974+TimKaspar@users.noreply.github.com>
2022-12-08 16:27:10 +0100

    trying db insert after auth
    this does not work

--- src/oauth2/index.js
+++ src/oauth2/index.js
@@ -1,8 +1,10 @@
 const { request } = require("undici");
 const express = require("express");
+const User = require('../schemas/user')
 const { discordClientId, discordClientSecret, bungieClientId, bungieClientSecret, port, apiKey } = require("./config.json");
 const fs = require("fs");
 const https = require("https");
+const { Types } = require("mongoose");
 const options = {
   key: fs.readFileSync("C:/Users/kti/Documents/ssl-certificate/cert.key"),
   cert: fs.readFileSync("C:/Users/kti/Documents/ssl-certificate/cert.crt"),
@@ -10,6 +12,8 @@ const options = {
 
 const app = express();
 const server = https.createServer(options, app);
+let discordId = "1"
+let d2MembershipId = "1"
 
 app.get("/", async ({query}, response) =>{
   return response.sendFile("src/oauth2/index.html", { root: "." });
@@ -17,7 +21,6 @@ app.get("/", async ({query}, response) =>{
 
 app.get("/discord", async ({ query }, response) => {
   const { code } = query;
-
   if (code) {
     try {
       const tokenResponseData = await request("https://discord.com/api/oauth2/token", {
@@ -43,7 +46,7 @@ app.get("/discord", async ({ query }, response) => {
       }).catch(console.error);
 
       const response = await userResult.body.json();
-      const discordId = response["id"];
+      discordId = response["id"];
       console.log(`Discord id: ${discordId}`);
     } catch (error) {
       // NOTE: An unauthorized token will not throw an error
@@ -82,8 +85,15 @@ app.get("/bungie/", async ({ query }, response) => {
         }
       }).catch(console.error);
       const response = await userResult.body.json();
-      const destinyMembershipId = Object.values(response["Response"]["membershipOverrides"])[0]["membershipIdOverriding"]
-      console.log(`D2 membership id: ${destinyMembershipId}`)
+      d2MembershipId = Object.values(response["Response"]["membershipOverrides"])[0]["membershipIdOverriding"]
+
+
+      let userProfile = await new User({
+        _id: Types.ObjectId(),
+        discordId: discordId,
+        d2MembershipId: d2MembershipId
+      });
+      await userProfile.save().catch(console.error)
 
     } catch (error) {
       // NOTE: An unauthorized token will not throw an error
