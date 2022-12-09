47463f5dbc211c163c617c01c10f022700fc82a1
Tim <33898974+TimKaspar@users.noreply.github.com>
2022-12-09 10:15:25 +0100

    announce all registered users
    additionally tag first user

--- src/commands/commands/announce.js
+++ src/commands/commands/announce.js
@@ -1,5 +1,8 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 const {GUILDID, CHANNELID} = process.env;
+const mongoose = require('mongoose')
+const User = require("../../schemas/user");
+const { response } = require("express");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("announce")
@@ -11,18 +14,35 @@ module.exports = {
     const message = await interaction.deferReply({
       fetchReply: true
     });
-    const embed = new EmbedBuilder({
-      title: "Announcement",
-      fields: [{
-        name: "Announcement",
-        value: "so real"
-      }]
-    });
-    await channel.send({ embeds: [embed] }).catch(console.error);
 
-    await interaction.editReply({
-      content: "Announcement posted!"
+    getData(async (callback) => {
+        console.log(callback)
+
+        const embed = new EmbedBuilder({
+          title: "Registered Users",
+        });
+        callback.forEach((e) =>{
+          embed.addFields({
+            name: `discord id: ${e[`discordId`]}`,
+            value: `d2 id: ${e[`d2MembershipId`]}`
+          })
+        })
+
+        await channel.send({ embeds: [embed] }).catch(console.error);
+        await interaction.editReply({
+          content: `Announcement posted! \n <@${callback[0][`discordId`]}> is the first registered User! `
+        });
       }
     );
   }
+  }
+
+  getData = (callback) => {
+    User.find({}, function(err, result) {
+      if (err) {
+        console.error(err)
+      } else {
+        callback(result)
+      }
+    });
   }
