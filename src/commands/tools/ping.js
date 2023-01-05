2e477f438e4ec753576e4a69e710e74ff4517411
Tim <tim2kaspar@gmail.com>
2023-01-05 08:42:49 +0100

    testing logs

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -6,7 +6,7 @@ module.exports = {
             fetchReply: true
         });
 
-        console.log(`API Latency is ${client.ws.ping}, Client Ping is ${message.createdTimestamp - interaction.createdTimestamp}`)
+        console.log(`API Latency is ${client.ws.ping}, Client Ping is ${message.createdTimestamp - interaction.createdTimestamp}`);
         const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`;
         await interaction.editReply({
             content: newMessage
