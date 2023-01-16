b59ec5353eb53a51a737ea90ad6a9ef9b4967f71
Tim <tim2kaspar@gmail.com>
2023-01-16 13:50:27 +0100

    added options for mentor/raid for announce

--- src/commands/commands/announce.js
+++ src/commands/commands/announce.js
@@ -1,9 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder, ApplicationCommandOptionBase } = require("discord.js");
-const bungieAPI = require("../../api/bungieAPI");
-const raidReportAPI = require("../../api/raidReportAPI");
+const { SlashCommandBuilder } = require("discord.js");
 const { PermissionFlagsBits } = require("discord-api-types/v10");
 const { getData } = require("../../functions/helpers/db");
-const { convertTime } = require("../../functions/helpers/convertTime");
 const { getBest, createRaidMessage, createPlayersMessage } = require("../../functions/helpers/announceHelper");
 const { GUILD_ID, CHANNEL_ID } = process.env;
 
@@ -14,17 +11,18 @@ module.exports = {
     .setDMPermission(false)
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .addStringOption(option =>
-    option.setName("raid")
-      .setDescription("Specify a raid")
-      .setRequired(false)
-      .addChoices(
-        {name: "King's Fall", value: "kf"},
-        {name: "Vow of the Disciple", value: "vow"},
-        {name: "Vault of Glass", value: "vog"},
-        {name: "Deep Stone Crypt", value: "dsc"},
-        {name: "Garden of Salvation", value: "gos"},
-        {name: "Last Wish", value: "lw"},
-      )),
+      option.setName("only")
+        .setDescription("Specify a Raid or Mentor to announce on its own")
+        .setRequired(false)
+        .addChoices(
+          { name: "Mentor", value: "mentor" },
+          { name: "King's Fall", value: "kf" },
+          { name: "Vow of the Disciple", value: "vow" },
+          { name: "Vault of Glass", value: "vog" },
+          { name: "Deep Stone Crypt", value: "dsc" },
+          { name: "Garden of Salvation", value: "gos" },
+          { name: "Last Wish", value: "lw" }
+        )),
 
   async execute(interaction, client) {
     // Discord server and channel from .env
@@ -37,23 +35,50 @@ module.exports = {
       fetchReply: true
     });
 
-    console.log(interaction.options.get("raid"))
     // processing command message
     console.log("getting data for announcement...");
     getData(async (users) => {
       console.log("db data received, calculating PBs...");
       getBest(users, async (best) => {
         console.log("data for announcement ready!");
+
+        const mentorMessage = createPlayersMessage(users, best.playedUsersCountMonthly[1], best.playedUsersCountMonthly[0]);
+        const kfMessage = createRaidMessage(users, best.kf.players, best.kf.activityTime, "King's Fall", "https://i.imgur.com/ShWT8Nq.png");
+        const vowMessage = createRaidMessage(users, best.vow.players, best.vow.activityTime, "Vow of the disciple", "https://i.imgur.com/MSwQTW1.png");
+        const vogMessage = createRaidMessage(users, best.vog.players, best.vog.activityTime, "Vault of Glass", "https://i.imgur.com/dMcnYnq.png");
+        const dscMessage = createRaidMessage(users, best.dsc.players, best.dsc.activityTime, "Deep Stone Crypt", "https://i.imgur.com/y603L7T.png");
+        const gosMessage = createRaidMessage(users, best.gos.players, best.gos.activityTime, "Garden of Salvation", "https://i.imgur.com/EBfhOzf.png");
+        const lwMessage = createRaidMessage(users, best.lw.players, best.lw.activityTime, "Last Wish", "https://i.imgur.com/FMDARhw.png");
+
         // create messages
-        const messageArray = [
-          createPlayersMessage(users, best.playedUsersCountMonthly[1], best.playedUsersCountMonthly[0]),
-          createRaidMessage(users, best.kf.players, best.kf.activityTime, "King's Fall", "https://i.imgur.com/ShWT8Nq.png"),
-          createRaidMessage(users, best.vow.players, best.vow.activityTime, "Vow of the disciple", "https://i.imgur.com/MSwQTW1.png"),
-          createRaidMessage(users, best.vog.players, best.vog.activityTime, "Vault of Glass", "https://i.imgur.com/dMcnYnq.png"),
-          createRaidMessage(users, best.dsc.players, best.dsc.activityTime, "Deep Stone Crypt", "https://i.imgur.com/y603L7T.png"),
-          createRaidMessage(users, best.gos.players, best.gos.activityTime, "Garden of Salvation", "https://i.imgur.com/EBfhOzf.png"),
-          createRaidMessage(users, best.lw.players, best.lw.activityTime, "Last Wish", "https://i.imgur.com/FMDARhw.png")
-        ];
+        let messageArray;
+        if (interaction.options.get("only") === null) {
+          messageArray = [mentorMessage, kfMessage, vowMessage, vogMessage, dscMessage, gosMessage, lwMessage];
+        } else {
+          switch (interaction.options.get("only").value) {
+            case "mentor":
+              messageArray = [mentorMessage];
+              break;
+            case "kf":
+              messageArray = [kfMessage];
+              break;
+            case "vow":
+              messageArray = [vowMessage];
+              break;
+            case "vog":
+              messageArray = [vogMessage];
+              break;
+            case "dsc":
+              messageArray = [dscMessage];
+              break;
+            case "gos":
+              messageArray = [gosMessage];
+              break;
+            case "lw":
+              messageArray = [lwMessage];
+              break;
+          }
+        }
 
         // send messages
         for (const message of messageArray) {
