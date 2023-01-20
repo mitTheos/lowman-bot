670a5417fcb1a9c0ffce18322aa204ccc6919646
Tim <tim2kaspar@gmail.com>
2023-01-20 12:27:10 +0100

    create free command

---
+++ src/commands/commands/free.js
@@ -0,0 +1,29 @@
+require("../../api/bungieAPI.js");
+require("../../api/raidReportAPI.js");
+const { SlashCommandBuilder, EmbedBuilder, ComponentBuilder, ButtonBuilder, ActionRowBuilder } = require("discord.js");
+const { ButtonStyle } = require("discord-api-types/v10");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("free")
+    .setDescription("free someone form jail")
+    .setDMPermission(false)
+    .addUserOption(option =>
+      option.setName("user").setDescription("specify a user to free up").setRequired(true)),
+  async execute(interaction, client) {
+    // loading message
+    console.log("===Free===");
+    await interaction.deferReply({
+      fetchReply: true
+    });
+if(interaction.options.get("user").value !== null){
+  await interaction.editReply({
+    content: `Free up <@${interaction.options.get("user").value}>, he's done nothing wrong🙏`
+  }).then(() => console.log("Posted message!"));
+} else {
+  await interaction.editReply({
+    content: `Invalid User!`
+  }).then(() => console.log("Invalid User!"));
+}
+  }
+};
