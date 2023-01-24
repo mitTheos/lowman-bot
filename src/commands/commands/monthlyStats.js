0d484a21170e15f0c5f0331488b71c6f291aa911
kti <tim2kaspar@gmail.com>
2023-01-24 14:07:33 +0100

    changed date to work same as announce
    instances are now collected for the current month instead of in last ~30 days

--- src/commands/commands/monthlyStats.js
+++ src/commands/commands/monthlyStats.js
@@ -1,87 +1,89 @@
-require("../../api/bungieAPI.js");
-require("../../api/raidReportAPI.js");
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const bungieAPI = require("../../api/bungieAPI.js");
-const raidReportAPI = require("../../api/raidReportAPI.js");
-const { PermissionFlagsBits } = require("discord-api-types/v10");
-const { convertTime } = require("../../functions/helpers/convertTime");
-const { getPlayedWith, MonthlyPB } = require("../../functions/helpers/monthlyStatsHelper");
+const {SlashCommandBuilder, EmbedBuilder} = require("discord.js");
+const {PermissionFlagsBits} = require("discord-api-types/v10");
+const {convertTime} = require("../../functions/helpers/convertTime");
+const {getPlayedWith} = require("../../functions/helpers/monthlyStatsHelper");
+const {
+    vowEmoji_id, vowEmoji_name, kfEmoji_name, kfEmoji_id, vogEmoji_name, vogEmoji_id, dscEmoji_id, dscEmoji_name, gosEmoji_name, gosEmoji_id, wishEmoji_id,
+    wishEmoji_name
+} = require("../../config/emojis");
+const {getMessageDate} = require("../../functions/helpers/announceHelper");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("monthly-stats")
-    .setDescription("Return stats on the past month")
-    .addStringOption((option) =>
-      option
-        .setName("username")
-        .setDescription("Bungie username")
-        .setRequired(true)
-    )
-    .setDMPermission(false)
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
-  async execute(interaction, client) {
+    data: new SlashCommandBuilder()
+        .setName("monthly-stats")
+        .setDescription("Return stats on the past month")
+        .addStringOption((option) =>
+            option
+                .setName("username")
+                .setDescription("Bungie username")
+                .setRequired(true)
+        )
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+    async execute(interaction, client) {
 
-    const username = interaction.options.get("username").value;
+        // loading message
+        console.log("===MonthlyStats===");
+        const message = await interaction.deferReply({
+            fetchReply: true,
+        });
 
-    // loading message
-    console.log("===MonthlyStats===");
-    const message = await interaction.deferReply({
-      fetchReply: true
-    });
-    getPlayedWith(username, async (callback) => {
-      console.log(`Getting monthly stats for: ${username}`);
-      const playedWithList = callback[0];
-      const lowmanList = callback[1];
-      const pb = new MonthlyPB(lowmanList);
+        const username = interaction.options.get("username").value;
+        getPlayedWith(username, async (callback) => {
+            if (callback !== null) {
+                const mentorEmbed = new EmbedBuilder()
+                    .setTitle(`Mentor stats (${getMessageDate(0)})`)
+                    .setColor(0xfa5c04)
+                    .addFields([
+                        {
+                            name: "Players you've played with",
+                            value: callback.playerCount.toString()
+                        }
+                    ]);
+                const raidEmbed = new EmbedBuilder()
+                    .setTitle(`Fastest lowmans (${getMessageDate(0)})`)
+                    .setColor(0xfa5c04)
+                    .addFields([
+                        {
+                            name: `<:${kfEmoji_name}:${kfEmoji_id}> KF`,
+                            value: convertTime(callback.pb.kf),
+                            inline: true
+                        },
+                        {
+                            name: `<:${vowEmoji_name}:${vowEmoji_id}>VOW`,
+                            value: convertTime(callback.pb.vow),
+                            inline: true
+                        },
+                        {
+                            name: `<:${vogEmoji_name}:${vogEmoji_id}> VOG`,
+                            value: convertTime(callback.pb.vog),
+                            inline: true
+                        },
+                        {
+                            name: `<:${dscEmoji_name}:${dscEmoji_id}> DSC`,
+                            value: convertTime(callback.pb.dsc),
+                            inline: true
+                        },
+                        {
+                            name: `<:${gosEmoji_name}:${gosEmoji_id}> GOS`,
+                            value: convertTime(callback.pb.gos),
+                            inline: true
+                        },
+                        {
+                            name: `<:${wishEmoji_name}:${wishEmoji_id}> LW`,
+                            value: convertTime(callback.pb.lw),
+                            inline: true
+                        }
+                    ]);
 
-      const embed = new EmbedBuilder()
-        .setTitle("Players you've played with this month")
-        .setColor(0xfa5c04)
-        .addFields([
-          {
-            name: "Players",
-            value: playedWithList.length.toString()
-          }
-        ]);
-      const embed2 = new EmbedBuilder()
-        .setTitle("Fastest lowmans this month")
-        .setColor(0xfa5c04)
-        .addFields([
-          {
-            name: "KF",
-            value: convertTime(pb.kf),
-            inline: true
-          },
-          {
-            name: "VOW",
-            value: convertTime(pb.vow),
-            inline: true
-          },
-          {
-            name: "VOG",
-            value: convertTime(pb.vog),
-            inline: true
-          },
-          {
-            name: "DSC",
-            value: convertTime(pb.dsc),
-            inline: true
-          },
-          {
-            name: "GOS",
-            value: convertTime(pb.gos),
-            inline: true
-          },
-          {
-            name: "LW",
-            value: convertTime(pb.lw),
-            inline: true
-          }
-        ]);
-
-      await interaction.editReply({
-        embeds: [embed, embed2]
-      }).then(() => console.log("Monthly Stats posted!"));
-    });
-  }
+                await interaction.editReply({
+                    embeds: [mentorEmbed, raidEmbed]
+                }).then(() => console.log("Monthly Stats posted!"));
+            } else {
+                await interaction.editReply({
+                    content: "No user was found with that username! Please try again"
+                }).then(() => console.log("Error, no user was found"));
+            }
+        });
+    }
 };
