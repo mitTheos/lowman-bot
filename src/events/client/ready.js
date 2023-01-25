4d1d95a635bd9cef9a562a08132bd990f84df2f9
Tim <tim2kaspar@gmail.com>
2023-01-25 12:39:02 +0100

    cache all members on startup
    this is to avoid wrong logs on first interaction when modifying a member's roles

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,10 @@
+const { guild_id } = require("../../config/guild");
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
+        const guild = await client.guilds.fetch(guild_id).catch(console.error);
+        guild.members.fetch().then((members) => members.cache)
         console.log(`Ready! ${client.user.tag} is logged in and online`);
     }
 }
