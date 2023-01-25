470f5a05f10bff8c63248e502d6a072bcc46bf6c
Tim <tim2kaspar@gmail.com>
2023-01-25 14:46:52 +0100

    format

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -1,17 +1,18 @@
-const {SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 const { PermissionFlagsBits } = require("discord-api-types/v10");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Return my ping').setDefaultMemberPermissions(PermissionFlagsBits.Administrator), async execute(interaction, client) {
-        console.log("===Ping===")
-        const message = await interaction.deferReply({
-            fetchReply: true
-        });
+  data: new SlashCommandBuilder().setName("ping").setDescription("Return my ping").setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  async execute(interaction, client) {
+    console.log("===Ping===");
+    const message = await interaction.deferReply({
+      fetchReply: true
+    });
 
-        console.log(`API Latency is ${client.ws.ping}, Client Ping is ${message.createdTimestamp - interaction.createdTimestamp}`);
-        const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`;
-        await interaction.editReply({
-            content: newMessage
-        }).then(console.log("Ping posted!"))
-    }
-}
+    console.log(`API Latency is ${client.ws.ping}, Client Ping is ${message.createdTimestamp - interaction.createdTimestamp}`);
+    const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`;
+    await interaction.editReply({
+      content: newMessage
+    }).then(() => console.log("Ping posted!"));
+  }
+};
