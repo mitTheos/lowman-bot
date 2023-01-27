966a25bdb89d951a5bace0825d60bf8c80acbc9d
Tim <tim2kaspar@gmail.com>
2023-01-27 16:04:48 +0100

    change monthlystats perms

--- src/commands/commands/monthlyStats.js
+++ src/commands/commands/monthlyStats.js
@@ -19,7 +19,7 @@ module.exports = {
                 .setRequired(true)
         )
         .setDMPermission(false)
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+        .setDefaultMemberPermissions(PermissionFlagsBits.AttachFiles),
     async execute(interaction, client) {
 
         // loading message
