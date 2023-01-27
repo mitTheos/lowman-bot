edc031135abbcb347733a3bc659b702eac3c84b8
Tim <tim2kaspar@gmail.com>
2023-01-27 11:11:17 +0100

    fix id for vog emoji

--- src/config/emojis.js
+++ src/config/emojis.js
@@ -12,4 +12,4 @@ exports.vowEmoji_id = "1065892654753263636";
 exports.dscEmoji_name = "dsc";
 exports.dscEmoji_id = "1065892684948054056";
 exports.vogEmoji_name = "vog";
-exports.vogEmoji_id = "1065892684948054056";
+exports.vogEmoji_id = "1065892641683820554";
