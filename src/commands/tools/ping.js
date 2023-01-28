6f3235bc954a8e9e5cc0567c8fc8873e508917ab
kti <tim2kaspar@gmail.com>
2023-01-28 02:09:17 +0100

    make ping ephemeral & change Perms

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -1,18 +1,19 @@
-const { SlashCommandBuilder } = require("discord.js");
-const { PermissionFlagsBits } = require("discord-api-types/v10");
+const {SlashCommandBuilder} = require("discord.js");
+const {PermissionFlagsBits} = require("discord-api-types/v10");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("Return my ping").setDefaultMemberPermissions(PermissionFlagsBits.ViewAuditLog),
-  async execute(interaction, client) {
-    console.log("===Ping===");
-    const message = await interaction.deferReply({
-      fetchReply: true
-    });
+    data: new SlashCommandBuilder().setName("ping").setDescription("Return my ping").setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+    async execute(interaction, client) {
+        console.log("===Ping===");
+        const message = await interaction.deferReply({
+            fetchReply: true,
+            ephemeral: true
+        });
 
-    console.log(`API Latency is ${client.ws.ping}, Client Ping is ${message.createdTimestamp - interaction.createdTimestamp}`);
-    const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`;
-    await interaction.editReply({
-      content: newMessage
-    }).then(() => console.log("Ping posted!"));
-  }
+        console.log(`API Latency is ${client.ws.ping}, Client Ping is ${message.createdTimestamp - interaction.createdTimestamp}`);
+        const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`;
+        await interaction.editReply({
+            content: newMessage
+        }).then(() => console.log("Ping posted!"));
+    }
 };
