0edd74f5e0c4ec6802ffa02f8988c8a4231674c7
kti <tim2kaspar@gmail.com>
2023-01-31 18:16:25 +0100

    change role perms

--- src/commands/commands/roles.js
+++ src/commands/commands/roles.js
@@ -7,7 +7,7 @@ module.exports = {
     .setName("forceroles")
     .setDescription("Update or remove a user's current Lowman roles.")
     .setDMPermission(false)
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
     .addSubcommand(subcommand => subcommand.setName("refresh").setDescription("Manually refresh a user's current Lowman Roles.").addUserOption(option =>
       option.setName("user").setDescription("clear roles of specified user").setRequired(true))
     ).addSubcommand(subcommand => subcommand.setName("remove").setDescription("Manually remove a user's current Lowman Roles.").addUserOption(option =>
