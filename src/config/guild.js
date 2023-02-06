6f53f38f53cb4f859b6b0ae8c776784baceea50f
Tim <tim2kaspar@gmail.com>
2023-02-06 10:12:44 +0100

    update channel

--- src/config/guild.js
+++ src/config/guild.js
@@ -1,2 +1,2 @@
 exports.guild_id = '1042662691774406686' //LOWMAN HUB
-exports.announce_channel_id = '1068553450251632770' //test channel
+exports.announce_channel_id = '1068482259620986952' //announce channel
