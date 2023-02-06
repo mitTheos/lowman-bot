495af419aadea4947b7d33e534943915a3d7a8ec
Tim <tim2kaspar@gmail.com>
2023-02-06 10:11:06 +0100

    grammar

--- src/events/mongo/error.js
+++ src/events/mongo/error.js
@@ -3,6 +3,6 @@ const chalk = require('chalk')
 module.exports = {
   name: "error",
   async execute(error){
-    console.error(chalk.red(`An error occured with the database connection: \n${error}`));
+    console.error(chalk.red(`An error occurred with the database connection: \n${error}`));
   }
 }
