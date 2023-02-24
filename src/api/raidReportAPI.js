503f584de8c3403a1eb3d3558c6004f7af282f48
Tim <tim2kaspar@gmail.com>
2023-02-24 08:58:45 +0100

    update error handling

--- src/api/raidReportAPI.js
+++ src/api/raidReportAPI.js
@@ -8,16 +8,16 @@ async function search(username) {
     method: "get",
     url: encodedURL,
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9'
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9"
     }
   };
   let data = null;
   try {
     await axios(config)
-        .then(function(response) {
-          data = response.data;
-        });
+      .then(function(response) {
+        data = response.data;
+      });
   } catch (err) {
     console.error(err);
   }
@@ -29,21 +29,32 @@ async function raidStats(membershipId) {
     method: "get",
     url: `https://api.raidreport.dev/raid/player/${membershipId}`,
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9'
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9"
     }
   };
 
   let data = null;
-  try {
-    await axios(config)
-        .then(function(response) {
-          data = response.data;
-        });
-  } catch (err) {
-    console.error("!!== Axios request error on raid/player ==!!")
-    console.error(err.response.statusCode + " " +  err.response.statusMessage);
-    console.error(err.response.url);
-  }
+  await axios(config)
+    .then(function(response) {
+      data = response.data;
+    }).catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
   return data;
 }
