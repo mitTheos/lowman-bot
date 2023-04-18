21886bbea073848310f981c9ddcada78520c42b5
Tim <tim2kaspar@gmail.com>
2023-04-18 10:47:12 +0200

    copy postman config

--- src/api/raidReportAPI.js
+++ src/api/raidReportAPI.js
@@ -26,13 +26,22 @@ async function search(username) {
 }
 
 async function raidStats(membershipId) {
-  const config = {
-    withCredentials: true,
-    method: "get",
-    url: `https://api.raidreport.dev/raid/player/${membershipId}`,
+  // const config = {
+  //   withCredentials: true,
+  //   method: "get",
+  //   url: `https://api.raidreport.dev/raid/player/${membershipId}`,
+  //   headers: {
+  //     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0",
+  //     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9"
+  //   }
+  // };
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://api.raidreport.dev/raid/player/4611686018501463948',
     headers: {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9'
     }
   };
 
