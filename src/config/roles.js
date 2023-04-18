96bb951f39147612d6d842ae057522fb5dad6852
Tim <tim2kaspar@gmail.com>
2023-04-18 14:48:36 +0200

    remove done comments

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,58 +1,58 @@
 //flawless
-exports.legendF_id = "1064527446831009792"; //done
-exports.masterF_id = "1064527490770538566"; //done
+exports.legendF_id = "1064527446831009792";
+exports.masterF_id = "1064527490770538566";
 
 //kf
-exports.kfTrio_id = `1063101506917765191`; //done
-exports.kfTrioF_id = `1063101543575978074`; //done 2.0
-exports.kfDuo_id = `1063101578623582270`; //done
-exports.kfTrioMF_id = `1063375357689995315`; //done 2.0
+exports.kfTrio_id = `1063101506917765191`;
+exports.kfTrioF_id = `1063101543575978074`;
+exports.kfDuo_id = `1063101578623582270`;
+exports.kfTrioMF_id = `1063375357689995315`;
 
 //vow
-exports.vowTrio_id = `1063101620889604267`; //done
-exports.vowTrioF_id = `1063101662794883132`; //done 2.0
-exports.vowTrioMF_id = `1063375418947797002`; //done 2.0
+exports.vowTrio_id = `1063101620889604267`;
+exports.vowTrioF_id = `1063101662794883132`;
+exports.vowTrioMF_id = `1063375418947797002`;
 
 //Vog
-exports.vogTrio_id = `1063056843594805278`; //done
-exports.vogTrioF_id = `1063056885080657993`; //done 2.0
-exports.vogDuo_id = `1063056947189911602`; //done
-exports.vogDuoF_id = `1063056975325315102`; //done 2.0
-exports.vogTrioMF_id = `1063375456683962428`; //done 2.0
-exports.vogDuoMF_id = `1063375498987700274`; //done 2.0
-exports.vogSolo_id = `1064135581031997511`; //Atheon, done
+exports.vogTrio_id = `1063056843594805278`;
+exports.vogTrioF_id = `1063056885080657993`;
+exports.vogDuo_id = `1063056947189911602`;
+exports.vogDuoF_id = `1063056975325315102`;
+exports.vogTrioMF_id = `1063375456683962428`;
+exports.vogDuoMF_id = `1063375498987700274`;
+exports.vogSolo_id = `1064135581031997511`;
 
 //dsc
-exports.dscTrio_id = `1063101719250215013`; //done
-exports.dscTrioF_id = `1063101778922569798`; //done 2.0
-exports.dscDuo_id = `1063101808274321438`; //done
-exports.dscDuoF_id = `1063101838276186112`; //done 2.0
+exports.dscTrio_id = `1063101719250215013`;
+exports.dscTrioF_id = `1063101778922569798`;
+exports.dscDuo_id = `1063101808274321438`;
+exports.dscDuoF_id = `1063101838276186112`;
 
 //gos
-exports.gosTrio_id = `1063101882739990599`; //done
-exports.gosTrioF_id = `1063101959546089502`; //done 2.0
-exports.gosDuo_id = `1063101994136518698`; //done
+exports.gosTrio_id = `1063101882739990599`;
+exports.gosTrioF_id = `1063101959546089502`;
+exports.gosDuo_id = `1063101994136518698`;
 
 //lw
-exports.lwTrio_id = `1063102039363702895`; //done
-exports.lwTrioF_id = `1063102087673679882`; //done 2.0
-exports.lwDuo_id = `1063102121035186186`; //done
-exports.lwSolo_id = `1064135645028683816`; //done
+exports.lwTrio_id = `1063102039363702895`;
+exports.lwTrioF_id = `1063102087673679882`;
+exports.lwDuo_id = `1063102121035186186`;
+exports.lwSolo_id = `1064135645028683816`;
 
 //crown
-exports.crownDuoF_id = `1068533367831810181`; //done
-exports.crownTrioF_id = `1068533457300508764`; //done
+exports.crownDuoF_id = `1068533367831810181`;
+exports.crownTrioF_id = `1068533457300508764`;
 
 //scourge
-exports.scourgeDuo_id = `1068533575680528476`; //done
-exports.scourgeTrioF_id = `1068533749890957423`; //done
+exports.scourgeDuo_id = `1068533575680528476`;
+exports.scourgeTrioF_id = `1068533749890957423`;
 
 //levi
-exports.leviDuo_id = `1068533626284802050`; //done
+exports.leviDuo_id = `1068533626284802050`;
 
 //eow
-exports.eowSolo_id = `1064135672023220284`; //done
-exports.eowDuo_id = `1097781487383883846`; //done
+exports.eowSolo_id = `1064135672023220284`;
+exports.eowDuo_id = `1097781487383883846`;
 
 //monthly roles
 exports.fast_id = `1097778753796579478`;
