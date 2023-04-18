52fe4a12dabe250a64bc6396e53cdd50866d2bdd
Tim <tim2kaspar@gmail.com>
2023-04-18 10:49:44 +0200

    copy full postman request

--- src/api/raidReportAPI.js
+++ src/api/raidReportAPI.js
@@ -26,15 +26,6 @@ async function search(username) {
 }
 
 async function raidStats(membershipId) {
-  // const config = {
-  //   withCredentials: true,
-  //   method: "get",
-  //   url: `https://api.raidreport.dev/raid/player/${membershipId}`,
-  //   headers: {
-  //     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0",
-  //     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9"
-  //   }
-  // };
   let config = {
     method: 'get',
     maxBodyLength: Infinity,
@@ -45,27 +36,12 @@ async function raidStats(membershipId) {
     }
   };
 
-  let data = null;
-  await axios(config)
-    .then(function(response) {
-      data = response.data;
-    }).catch(function(error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
+  axios.request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      return (JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  return data;
 }
